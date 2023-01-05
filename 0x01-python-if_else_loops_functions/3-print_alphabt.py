293bfe43550676f71413686482937106bb8216c6
Lindarose247 <ndlovulindarose@gmail.com>
2023-01-04 18:47:22 -0800

    if else loops function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
+for i in range(97, 123):
 
-    if chr(letter) is not 'q' and chr(letter) is not 'e':
+    if chr(i) == 'q' or chr(i) == 'e':
 
-        print("{}".format(chr(letter)), end="")
+        continue
+
+    print(chr(i).format(i), end='')
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def print_last_digit(number):
 
-    """Print the last digit of a number and return it."""
+    '''prints the last digit of a number'''
 
-    print(abs(number) % 10, end="")
+    last_digit = abs(number) % 10
 
-    return (abs(number) % 10)
+    print(f"{last_digit}", end='')
+
+    return last_digit
