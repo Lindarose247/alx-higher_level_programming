90b33726b26d831daf77fa3f69ed2e4c74153cdc
Lindarose247 <ndlovulindarose@gmail.com>
2023-04-12 03:20:49 +0200

    Javascript introduction

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+function printArguments(arg1, arg2) {
+  // Check if both arguments are provided
+  if (arg1 === undefined || arg2 === undefined) {
+    console.error('Error: Two arguments must be provided');
+    return;
+  }
+
+  // Print arguments in the required format
+  console.log(`${arg1} is ${arg2}`);
+}
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const printCIsFun = (x) => {
+const printingCIsFun = (x) => {
   if (isNaN(x)) {
-    console.log("Missing number of occurrences");
+    console.log('Missing number of occurrences');
   } else {
     let i = 0;
     while (i < x) {
-      console.log("C is fun");
+      console.log('C is fun');
       i++;
     }
   }
